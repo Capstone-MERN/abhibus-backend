5ad6d792fcfb415af1d8cd87fe4592e5ca60186c
dhruv <dhruvpatel9755@gmail.com>
2024-05-13 17:41:58 +0530

    separated lower and upper_deck schemas

--- models/busSchema.js
+++ models/busSchema.js
@@ -1,34 +1,29 @@
 const mongoose = require("mongoose");
 const constants = require("./constants");
+
+//created seatSchema
+const seatSchema = {
+  row: { type: Number, required: true },
+  column: { type: Number, required: true },
+  seatNumber: { type: String, required: true },
+  seatType: {
+    type: String,
+    enum: constants.seatTypes,
+    required: true,
+  },
+};
+
+//separated lower and upper deck schema
+const lowerDeckSchema = [seatSchema];
+const upperDeckSchema = [seatSchema];
+
 const busSchema = new mongoose.Schema({
   id: { type: Number, unique: true, required: true },
   plateNumber: { type: String, required: true },
   busType: { type: String, enum: constants.busTypes, required: true },
   seatLayout: {
-    lower_deck: [
-      {
-        row: { type: Number, required: true },
-        column: { type: Number, required: true },
-        seatNumber: { type: String, required: true },
-        seatType: {
-          type: String,
-          enum: constants.seatTypes,
-          required: true,
-        },
-      },
-    ],
-    upper_deck: [
-      {
-        row: { type: Number, required: true },
-        column: { type: Number, required: true },
-        seatNumber: { type: String, required: true },
-        seatType: {
-          type: String,
-          enum: constants.seatTypes,
-          required: true,
-        },
-      },
-    ],
+    lower_deck: lowerDeckSchema,
+    upper_deck: upperDeckSchema,
   },
   partner: { type: String, required: true },
   amenities: [
--- models/constants.js
+++ models/constants.js
@@ -1,16 +1,7 @@
 const seatTypes = ["SLEEPER", "SEATER", "SEMI_SLEEPER"];
-const tourPlaces = {
-  cityId: { type: Number, required: true },
-  stops: [
-    {
-      stopId: { type: Number, required: true },
-      arrivalTime: { type: Number, required: true },
-    },
-  ],
-};
+
 const busTypes = ["AC", "NON_AC"];
 module.exports = {
   seatTypes: seatTypes,
-  tourPlaces: tourPlaces,
   busTypes: busTypes,
 };
