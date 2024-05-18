ee9a7aafe7a7b897ebd4dac31888e661310a7e4c
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2024-05-18 13:03:59 +0530

    minor code refactor

--- src/services/tourService.js
+++ src/services/tourService.js
@@ -1,140 +1,128 @@
-const City = require('../models/CitySchema');
-const Bus = require('../models/busSchema')
-const Tour = require('../models/tourSchema')
-const Booking = require('../models/bookingSchema')
-
-const findStopPointsByCityId = async (cityId) => {
-    try {
-        const city = await City.findOne({ cityId: cityId });
-        if (!city) {
-            throw new Error('City not found');
-        }
-        return city.stops.map(stop => ({
-            stopId: stop.stopId,
-            directions : stop.directions,
-            name: stop.name
-        }));
-    } catch (error) {
-        throw new Error(`Error finding stop points for cityId ${cityId}: ${error.message}`);
-    }
-};
-
-
-const findCityName = async(cityId) => {
-    try{
-      const city = await City.findOne({ cityId: cityId }, { name: 1 });
-    if(!city){
-        throw new Error('City not found');
-    }
-    return city.name;
-    } catch(error){
-      throw new Error(`Error finding the city for cityId ${cityId} : ${error.message}`);
-    }
-}    
-
-
+const City = require("../models/CitySchema");
+const Bus = require("../models/busSchema");
+const Tour = require("../models/tourSchema");
+const Booking = require("../models/bookingSchema");
 
 const calculateDuration = (startTime, endTime) => {
-    const duration = endTime - startTime; 
-    return duration / (1000 * 60); 
-};
-  
-  
-const calculateAvailableSeats = async (tourId, bus) => {
-    const bookings = await Booking.find({ tourId });
-    const bookedSeats = bookings.reduce((acc, booking) => acc + booking.bookedSeats.length, 0);
-    const totalSeats = bus.seatLayout.lowerDeck.length + bus.seatLayout.upperDeck.length;
-    return totalSeats - bookedSeats;
+  // TODO: convert the seconds into `2d 3h 50m`
+  const duration = endTime - startTime;
+  return duration;
 };
 
+const getBookedSeatsByTourId = async (tourId) => {
+  const bookings = await Booking.find({ tourId }, { bookedSeats: 1 });
+  const bookedSeats = bookings.reduce(
+    (acc, booking) => acc + booking.bookedSeats.length,
+    0
+  );
+  return bookedSeats;
+};
 
 const findTours = async (cityId1, cityId2, travelDate) => {
-    try {
-      
-      const startOfDay = new Date(travelDate * 1000).setHours(0, 0, 0, 0) / 1000;
-      const endOfDay = new Date(travelDate * 1000).setHours(23, 59, 59, 999) / 1000;
-  
-      // current epoch time
-      const currentTime = Math.floor(Date.now() / 1000);
+  try {
+    // FIXME: everything should be dealed with millis.
+    const startOfDay = new Date(travelDate * 1000).setHours(0, 0, 0, 0) / 1000;
+    const endOfDay =
+      new Date(travelDate * 1000).setHours(23, 59, 59, 999) / 1000;
 
-      let query = {
-        'source.cityId': cityId1,
-        'destination.cityId': cityId2,
-        travelDate: { $gte: startOfDay, $lte: endOfDay }
-      };
-  
-      // If the travel date is today
-      const today = Math.floor(new Date().setHours(0, 0, 0, 0) / 1000);
-      if (travelDate === today) {
-        query['source.stops.0.arrivalTime'] = { $gte: currentTime };
-      }
-  
-      const tours = await Tour.find(query);
-  
-      const results = [];
-  
-      for (const tour of tours) {
-        //  bus data
-        const bus = await Bus.findOne({ id: tour.busId });
-        // bookings for the tour
-        const availableSeats = await calculateAvailableSeats(tour.id, bus);
-        
-        
-          const response = {
-            busId: tour.busId,
-            tourId: tour.id,
-            busType: bus.busType,
-            busPartner: bus.partner,
-            amenities: bus.amenities.map(a => a.label),
-            startTime: tour.source.stops[0].arrivalTime,
-            endTime: tour.destination.stops[0].arrivalTime,
-            duration: calculateDuration(tour.source.stops[0].arrivalTime, tour.destination.stops[0].arrivalTime),
-            availableSeats,
-            minPrice: Math.min(...tour.prices.map(p => p.price)),
-            maxPrice: Math.max(...tour.prices.map(p => p.price)),
-            sourceStops: tour.source.stops,
-            destinationStops: tour.destination.stops,
-          };
-  
-        results.push(response);
-      
+    // current epoch time
+    const currentTime = Math.floor(Date.now() / 1000);
+
+    let query = {
+      "source.cityId": cityId1,
+      "destination.cityId": cityId2,
+      travelDate: { $gte: startOfDay, $lte: endOfDay },
+    };
+
+    // If the travel date is today
+    const today = Math.floor(new Date().setHours(0, 0, 0, 0) / 1000);
+    if (travelDate === today) {
+      query["source.stops.0.arrivalTime"] = { $gte: currentTime };
     }
-      return results;
-    } catch (error) {
-      console.error('Error fetching tours:', error);
-      throw error;
-    }
-  };
 
+    const tours = await Tour.find(query);
+    const busIds = tours.map((tour) => tour.busId);
 
-const getResponse = async(sourceCityId, destinationCityId, date) => {
-  const sourceCity = await findCityName(sourceCityId) 
+    const results = [];
 
-      const destinationCity = await findCityName(destinationCityId)
+    const buses = await Bus.find(
+      { id: { $in: busIds } },
+      { busType: 1, partner: 1, amenities: 1, _id: 0, seatLayout: 1, id: 1 }
+    );
 
-      const tours = await findTours(sourceCityId, destinationCityId, date);
+    const busMapping = new Map();
+    buses.forEach((bus) => {
+      busMapping.set(bus.id, {
+        busType: bus.busType,
+        partner: bus.partner,
+        amenities: bus.amenities,
+        totalSeats:
+          (bus.seatLayout?.upperDeck?.length ?? 0) +
+          bus.seatLayout.lowerDeck.length,
+      });
+    });
 
-      const sourceStopPoints = await findStopPointsByCityId(sourceCityId);
+    for (const tour of tours) {
+      const bus = busMapping.get(tour.busId);
+      const bookedSeats = await getBookedSeatsByTourId(tour.id);
+      const availableSeats = bus.totalSeats - bookedSeats;
 
-      const destinationStopPoints = await findStopPointsByCityId(destinationCityId);
+      let minPrice = Number.MAX_SAFE_INTEGER,
+        maxPrice = 0;
+
+      tour.prices.forEach(({ price }) => {
+        if (price > maxPrice) maxPrice = price;
+        if (price < minPrice) minPrice = price;
+      });
 
       const response = {
-        sourceCity, 
-        destinationCity,
-        boardingPoints: sourceStopPoints,
-        droppingPoints: destinationStopPoints,
-        tours: tours
+        busId: tour.busId,
+        tourId: tour.id,
+        busType: bus.busType,
+        busPartner: bus.partner,
+        amenities: bus.amenities,
+        startTime: tour.source.stops[0].arrivalTime,
+        endTime: tour.destination.stops[0].arrivalTime,
+        duration: calculateDuration(
+          tour.source.stops[0].arrivalTime,
+          tour.destination.stops[0].arrivalTime
+        ),
+        availableSeats,
+        minPrice,
+        maxPrice,
+        sourceStops: tour.source.stops,
+        destinationStops: tour.destination.stops,
       };
-  
-      return response;
-}
 
-module.exports = {
-    findStopPointsByCityId, 
-    findTours,
-    findCityName,
-    getResponse
+      results.push(response);
+    }
+
+    return results;
+  } catch (error) {
+    console.error("Error fetching tours:", error);
+    throw error;
+  }
 };
 
+const getResponse = async (sourceCityId, destinationCityId, date) => {
+  const filter = { city: 1, stops: { stopId: 1, directions: 1, name: 1 } };
+  const sourceCity = await City.findOne({ cityId: sourceCityId }, filter);
+  const destinationCity = await City.findOne(
+    { cityId: destinationCityId },
+    filter
+  );
+  const tours = await findTours(sourceCityId, destinationCityId, date);
 
+  const response = {
+    sourceCity: sourceCity.city,
+    destinationCity: destinationCity.city,
+    // FIXME: exclude _id property for all the stops
+    boardingPoints: sourceCity.stops,
+    droppingPoints: destinationCity.stops,
+    tours: tours,
+  };
 
+  return response;
+};
+
+module.exports = { getResponse };
