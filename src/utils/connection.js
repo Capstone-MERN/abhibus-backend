0777c83aa43108391bf73730bcb87a66b525fe17
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2024-06-03 18:19:04 +0530

    updated log message

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDataBaseServer = async () => {
   try {
     await mongoose.connect(process.env.DB_HOST);
-    console.log("connected to mongoDB");
+    console.log("connected to mongoDB", process.env.DB_HOST);
   } catch (error) {
     console.error("Connection to mongoDB failed", error.message);
   }
